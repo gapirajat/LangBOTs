45b02c954bb628cf923367e4593b6392eb59e4d6
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2023-08-01 13:43:36 +0000

    3

--- scrapsplitvec.py
+++ scrapsplitvec.py
@@ -11,37 +11,39 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 # Python code to illustrate the Modules
 class SSV:
 
-	def __init__(self):
-		self.urls = ['https://it.pccoepune.com/','https://it.pccoepune.com/hod']
-        self.chunk_size = 600
-        self.chunk_overlap = 100
-        self.model_name = "hkunlp/instructor-xl"
-        self.kwargs = 3
+        def __init__(self):
+                self.urls = ['https://it.pccoepune.com/','https://it.pccoepune.com/hod']
+                self.chunk_size = 600
+                self.chunk_overlap = 100
+                self.model_name = "hkunlp/instructor-xl"
+                self.kwargs = 3
         
-        retriever = self.vec(self.split(self.scrap()))
-        return retriever
 
-	# A normal print function
-	def scrap(self):
-		loaders = UnstructuredURLLoader(urls=urls)
-        data = loaders.load()
-        return data
+        # A normal print function
+        def scrap(self):
+                loaders = UnstructuredURLLoader(urls=self.urls)
+                data = loaders.load()
+                return data
 
-	def split(self, data):
-        #initialised text splitter
-        text_splitter = CharacterTextSplitter(separator=' ',
+        def split(self, data):
+                #initialised text splitter
+                text_splitter = CharacterTextSplitter(separator=' ',
                                       chunk_size=self.chunk_size,
                                       chunk_overlap=self.chunk_overlap)
-        #split the text                              
-        docs = text_splitter.split_documents(data)                              
-        return docs                              
+                #split the text                              
+                docs = text_splitter.split_documents(data)                              
+                return docs                              
 
-	def vec(self, docs):
-        #downloading instructor embedding
-        instructor_embeddings = HuggingFaceInstructEmbeddings(model_name=self.model_name,
+        def vec(self, docs):
+                #downloading instructor embedding
+                instructor_embeddings = HuggingFaceInstructEmbeddings(model_name=self.model_name,
                                                       model_kwargs={"device": "cuda"})
-        #creating vector graph
-        db_instructEmbedd = FAISS.from_documents(docs, instructor_embeddings)
-        retriever = db_instructEmbedd.as_retriever(search_kwargs={"k": self.kwargs})
-        return retriever
+                #creating vector graph
+                db_instructEmbedd = FAISS.from_documents(docs, instructor_embeddings)
+                retriever = db_instructEmbedd.as_retriever(search_kwargs={"k": self.kwargs})
+                return retriever
+        
+        def retur(self):
+                retriever = self.vec(self.split(self.scrap()))
+                return retriever
 #urls, chunk size, overlap, search kwargrs, instructor model
