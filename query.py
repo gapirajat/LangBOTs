5c7ae6194224136cc83f32c2b6e17a3f0e658c66
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2023-08-01 14:11:53 +0000

    4

--- query.py
+++ query.py
@@ -5,28 +5,29 @@ import textwrap
 class Query:
 
     def __init__(self, query):
-        return self.qury(query)
+        ret = self.qury(query)
+        return ret
+    
+    def wrap_text_preserve_newlines(text, width=110):
+        # Split the input text into lines based on newline characters
+        lines = text.split('\n')
 
-	def wrap_text_preserve_newlines(text, width=110):
-		# Split the input text into lines based on newline characters
-		lines = text.split('\n')
+        # Wrap each line individually
+        wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
 
-		# Wrap each line individually
-		wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
+        # Join the wrapped lines back together using newline characters
+        wrapped_text = '\n'.join(wrapped_lines)
 
-		# Join the wrapped lines back together using newline characters
-		wrapped_text = '\n'.join(wrapped_lines)
+        return wrapped_text
 
-		return wrapped_text
+    def process_llm_response(llm_response):
+        print(self.wrap_text_preserve_newlines(llm_response['result']))
+        print('\nSources:')
+        for source in llm_response["source_documents"]:
+            return source.metadata['source']
 
-	def process_llm_response(llm_response):
-		print(wrap_text_preserve_newlines(llm_response['result']))
-		print('\nSources:')
-		for source in llm_response["source_documents"]:
-			return source.metadata['source']
-
-	def qury(query):
-		print('-------------------Instructor Embeddings------------------\n')
-		llm_response = qa_chain_instrucEmbed(query)
-		res = self.process_llm_response(llm_response)
-		return res
+    def qury(query):
+        print('-------------------Instructor Embeddings------------------\n')
+        llm_response = qa_chain_instrucEmbed(query)
+        res = self.process_llm_response(llm_response)
+        return res
