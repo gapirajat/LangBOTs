3eef2dc6420444b26ab79f5cff93b0a8995db344
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2023-08-01 14:18:35 +0000

    4

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import scrapsplitvec
 import chain
 import query
 
+from scrapsplitvec import SSV
+from chain import MDL
+from query import Query
+
+
 retriever = SSV()
 qa_chain_instrucEmbed = MDL(retriever = retriever)
 Query("hello")
