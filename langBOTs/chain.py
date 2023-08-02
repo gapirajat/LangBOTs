f0aca711c81f70afc94da54a65b6ea582affb688
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2023-08-02 07:27:10 +0000

    second release

--- langBOTs/chain.py
+++ langBOTs/chain.py
@@ -9,7 +9,7 @@ class MDL:
     model_id: str = "bigscience/bloom-560m"
     task: str = "text-generation"
     model_kwargs: dict = {"temperature": 0, "max_length": 512}
-    # device: int = 0
+    device: int = 0
     llm: any = HuggingFacePipeline.from_model_id(model_id, task, model_kwargs, device,)
 
     # def __init__(self, retriever, model_id = "bigscience/bloom-560m", task = "text-generation", model_kwargs = {"temperature": 0, "max_length": 512}, device = 0):
