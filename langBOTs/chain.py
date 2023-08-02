a4a380ea61dada72e52516fe93fc8fbb4678f237
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2023-08-02 10:58:29 +0000

    L

--- langBOTs/chain.py
+++ langBOTs/chain.py
@@ -10,7 +10,7 @@ class MDL(BaseModel):
     task: str = "text-generation"
     model_kwargs: Any = {"temperature": 0, "max_length": 512}
     device: int = -1
-    llm: Any = HuggingFacePipeline.from_model_id(model_id = model_id, task = task, model_kwargs = model_kwargs, device = device)
+    llm: Any = 1
 
     # def __init__(self, retriever, model_id = "bigscience/bloom-560m", task = "text-generation", model_kwargs = {"temperature": 0, "max_length": 512}, device = 0):
     #     self.retriever = retriever
@@ -23,6 +23,8 @@ class MDL(BaseModel):
 
     #initialising chains
     def chain(self):
+        if llm != 1:
+            self.llm = HuggingFacePipeline.from_model_id(model_id = self.model_id, task = self.task, model_kwargs = self.model_kwargs, device = self.device)
         qa_chain_instrucEmbed = RetrievalQA.from_chain_type(llm=self.llm,
                                                     chain_type="stuff",
                                                     retriever=self.retriever,
