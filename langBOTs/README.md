edc64f0ab8e3a192297a5fe2916df66f03fcee55
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2023-08-02 12:11:06 +0000

    First Release! really this time :3

--- langBOTs/README.md
+++ langBOTs/README.md
@@ -1,1 +1,2 @@
+A silly library which combines scraping, langchain and flask
 (scrape->load->split->[dwnld_instructor_e]->create_vec_graph->save(optional)->[download_model->]initialise_chain->query->)->api
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='langBOTs',
-    version='0.1.0',
+    version='0.0.1',
     author='Rajat S',
     description='Scrape, Langchain, Deploy !',
     packages=find_packages(),
