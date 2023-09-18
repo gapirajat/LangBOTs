af1fdf5cba0e607bdc57b6fec8417b833509f031
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2023-09-18 04:53:03 +0000

    modified query (was just giving source)

--- langBOTs/query.py
+++ langBOTs/query.py
@@ -5,10 +5,9 @@ from typing import Any
 
 class Query(BaseModel):
     chain: Any = None
-    
+
     def wrap_text_preserve_newlines(self, text, width=110):
         # Split the input text into lines based on newline characters
-        print(type(text))
         lines = text.split('\n')
 
         # Wrap each line individually
@@ -20,13 +19,13 @@ class Query(BaseModel):
         return wrapped_text
 
     def process_llm_response(self, llm_response):
-        print(self.wrap_text_preserve_newlines(llm_response['result']))
-        print('\nSources:')
-        for source in llm_response["source_documents"]:
-            return source.metadata['source']
+        # print(self.wrap_text_preserve_newlines(llm_response['result']))
+        print('\nResponse:')
+        # for source in llm_response["source_documents"]:
+        #     return source.metadata['source']
+        return self.wrap_text_preserve_newlines(llm_response['result'])
 
     def qury(self, query: str):
-        print('-------------------Instructor Embeddings------------------\n')
         llm_response = self.chain(query)
         res = self.process_llm_response(llm_response)
         return res
